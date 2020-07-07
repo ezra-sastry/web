1ed68af7088c0933718240466a922c211c22a025
Ezra Christian <ezrarnold@gmail.com>
2020-07-07 20:44:21 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openClose() {
+    openCloseOverlay();
+}
+
+function openCloseOverlay() {
+    document.querySelector(".menu-icon").classList.toggle("change");
+    document.querySelector(".overlay").classList.toggle("open");
+}
+
