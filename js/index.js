cdfbe0587ffba8eb3626fe2480d2531c520aab90
Ezra Christian <ezrarnold@gmail.com>
2020-07-09 15:17:48 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ function thankYou() {
     x.className = "showThanks"
     setTimeout(function(){x.className = x.className.replace("show", "");}, 3000);
 }
+
+function copyText() {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("desc-text"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+
+    var tooltipText = document.getElementById("copyTooltip");
+    tooltipText.innerHTML = "Copied";
+}
+
+function setCopyText() {
+    var tooltipText = document.getElementById("copyTooltip");
+    tooltipText.innerHTML = "Copy";
+}
